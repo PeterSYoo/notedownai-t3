869f99de96ae8fe80111c1898833f85e0181cc63
Peter Yoo <trustintofu@gmail.com>
2023-04-07 04:28:37 -0700

    style(Home): Changes gap, height and max width.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,11 @@ const Home: NextPage = () => {
       <main className="flex h-full w-full flex-grow bg-gradient-to-tr from-[#2A2B2E] to-[#3E3E42]">
         <div className="flex flex-grow bg-[url('https://res.cloudinary.com/dryh1nvhk/image/upload/v1680749458/NoteDown%20AI/landing-bg_n5bcpn.jpg')] bg-cover bg-center bg-no-repeat">
           <div className="flex h-full w-full items-center justify-center overflow-hidden">
-            <div className="flex h-full w-full flex-col items-center gap-3 rounded-lg bg-white/10 backdrop-blur-sm">
-              <h1 className="flex justify-center pt-5 text-5xl font-extrabold text-[#313133]">
+            <div className="flex h-full w-full flex-col items-center gap-5 rounded-lg bg-white/10 backdrop-blur-sm">
+              <h1 className="flex justify-center pt-12 text-5xl font-extrabold text-[#313133]">
                 NoteDown AI
               </h1>
-              <p className="h-[50px] max-w-[340px] text-center">
+              <p className="h-[35px] max-w-[300px] text-center">
                 <TypingText
                   texts={[
                     "Our app employs generative ai that automatically takes notes for you, saving you time and effort.",
