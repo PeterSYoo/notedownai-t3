8827a565eb353890d253c636baa8fcf4994fbebc
Peter Yoo <trustintofu@gmail.com>
2023-04-07 00:36:48 -0700

    feat(EmailExistsModal): Creates modal for email exists.

---
+++ src/components/landing-page/signup-tab/EmailExistsModal.components.tsx
@@ -0,0 +1,78 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { useEffect, useRef, useState } from "react";
+
+export const EmailExistsModal = ({
+  setIsEmailExistsModal,
+}: {
+  setIsEmailExistsModal: (arg0: boolean) => void;
+}) => {
+  // ------------------------------------- States ------------------------------------ ***
+  const [scale, setScale] = useState<number>(0.9);
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // ------------------------------- Custom Functions -------------------------------- ***
+  const handleClickOutsideModal = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      setIsEmailExistsModal(false);
+    }
+  };
+
+  // ------------------------------------ Effects ------------------------------------ ***
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutsideModal);
+    return () => {
+      document.removeEventListener("click", handleClickOutsideModal);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsEmailExistsModal(false);
+        setScale(0.9);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScale(1);
+    }, 1);
+  }, []);
+
+  // -------------------------------------- JSX -------------------------------------- ***
+  return (
+    <>
+      <section className="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-white/20">
+        {/* -------------------------- Modal Container ------------------------------- */}
+        <div
+          ref={modalRef}
+          className="h-fit w-fit rounded-xl bg-[#232325] px-5 pt-3 transition-all duration-300"
+          style={{
+            transform: `translate(0%, 0%) scale(${scale})`,
+          }}
+        >
+          <div className="flex w-full justify-end">
+            <button onClick={() => setIsEmailExistsModal(false)}>
+              <Cross2Icon className="h-[17px] w-[17px] text-white/70 hover:text-white" />
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2 pb-7 text-white/70">
+            <h1 className="font-bold">Email Already Exists!</h1>
+            <p className="max-w-[300px] px-2 text-center text-sm">
+              Someone already registered with that email.
+            </p>
+          </div>
+        </div>
+        {/* -------------------------------------------------------------------------- */}
+      </section>
+    </>
+  );
+};
