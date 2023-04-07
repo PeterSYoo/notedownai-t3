5ad3cb15b7c8ce4bf40bc8dee23435536195c6a0
Peter Yoo <trustintofu@gmail.com>
2023-04-06 20:49:50 -0700

    feat(middleware): Creates middleware to handle redirects by checking next auth cookies.

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { type NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: ["/", "/((?!_next/static)(?!api)(?!favicon.ico).*)"],
+};
+
+export function middleware(req: NextRequest) {
+  const cookieName = req.cookies.get("next-auth.session-token")
+    ? "next-auth.session-token"
+    : "__Secure-next-auth.session-token";
+  const cookieValue = req.cookies.get(cookieName)?.value;
+
+  if (cookieValue === undefined) {
+    if (req.nextUrl.pathname !== "/") {
+      const url = req.nextUrl.clone();
+      url.pathname = "/";
+      return NextResponse.redirect(url, 301);
+    }
+  } else if (cookieValue !== undefined) {
+    if (req.nextUrl.pathname === "/") {
+      const url = req.nextUrl.clone();
+      url.pathname = "/dashboard";
+      return NextResponse.redirect(url, 301);
+    } else {
+      return null;
+    }
+  }
+}
