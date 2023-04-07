a2c896c6836f933b178e24ba34a9448d28b529ba
Peter Yoo <trustintofu@gmail.com>
2023-04-06 21:41:23 -0700

    feat(LoginTab): Uses zod to validate the fields, refactors the popovers to show on hover, dynamically updates the error message in the popovers based on validation errors.

--- src/components/landing-page/LoginTab.components.tsx
+++ src/components/landing-page/LoginTab.components.tsx
@@ -6,16 +6,66 @@ import {
 import { useState } from "react";
 import * as Popover from "@radix-ui/react-popover";
 import { AuthProviders } from "./AuthProviders.components";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+// ---------------------------------- Form Schemas ----------------------------------- ***
+const validationSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Must contain at least 3 characters.")
+    .max(12, "Cannot be more than 12 characters.")
+    .regex(
+      /^[a-zA-Z0-9]+$/,
+      "Can only contain letters and numbers, no spaces."
+    ),
+  password: z
+    .string()
+    .min(6, "Must contain at least 6 characters.")
+    .max(20, "Cannot be more than 20 characters.")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?!.*\s).*$/,
+      "Password must contain at least 1 uppercase, 1 lowercase, 1 special character(s) and no spaces."
+    ),
+});
+
+type ValidationSchema = z.infer<typeof validationSchema>;
+
+// -------------------------------- Component Function ------------------------------- ***
 export const LoginTab = () => {
   // ------------------------------------- States ------------------------------------ ***
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [popoverUsername, setPopoverUsername] = useState<boolean>(false);
+  const [popoverPassword, setPopoverPassword] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<ValidationSchema>({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    resolver: zodResolver(validationSchema),
+  });
+
+  // ------------------------------- Custom Functions -------------------------------- ***
+  const onSubmit: SubmitHandler<ValidationSchema> = (data) => console.log(data);
+
+  const handleHover = (popover: string) => {
+    if (popover === "username") setPopoverUsername(true);
+    if (popover === "password") setPopoverPassword(true);
+  };
+  const handleLeave = (popover: string) => {
+    if (popover === "username") setPopoverUsername(false);
+    if (popover === "password") setPopoverPassword(false);
+  };
 
   // -------------------------------------- JSX -------------------------------------- ***
   return (
     <>
       <section className="flex flex-col gap-5 px-12 pt-10">
-        <form className="flex flex-col gap-2">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
           {/* --------------------------- Username ----------------------------------- */}
           <fieldset className="flex flex-col gap-1">
             <label
@@ -23,15 +73,19 @@ export const LoginTab = () => {
               htmlFor="username"
             >
               Username
-              <Popover.Root>
+              <Popover.Root
+                open={popoverUsername}
+                onOpenChange={setPopoverUsername}
+              >
                 <Popover.Trigger asChild>
-                  <button
-                    type="button"
-                    className=""
-                    aria-label="Username requirements"
-                  >
-                    <InfoCircledIcon className="h-[17px] w-[17px]" />
-                  </button>
+                  {errors.username && (
+                    <InfoCircledIcon
+                      className="h-[17px] w-[17px] text-red-500"
+                      onMouseEnter={() => handleHover("username")}
+                      onMouseLeave={() => handleLeave("username")}
+                      aria-label="Username requirements"
+                    />
+                  )}
                 </Popover.Trigger>
                 <Popover.Portal>
                   <Popover.Content className="PopoverContent">
@@ -45,8 +99,10 @@ export const LoginTab = () => {
               </Popover.Root>
             </label>
             <input
-              className="h-[35px] rounded-lg border border-white/20 bg-transparent px-2 text-white/50 focus:outline-none"
-              id="name"
+              className={`h-[35px] rounded-lg border border-white/20 bg-transparent px-2 text-white/50 focus:outline-none ${
+                (errors.username && "border-red-600/90 text-red-500") || ""
+              }`}
+              {...register("username")}
             />
           </fieldset>
           {/* ------------------------------------------------------------------------ */}
@@ -57,15 +113,19 @@ export const LoginTab = () => {
               htmlFor="username"
             >
               Password
-              <Popover.Root>
+              <Popover.Root
+                open={popoverPassword}
+                onOpenChange={setPopoverPassword}
+              >
                 <Popover.Trigger asChild>
-                  <button
-                    type="button"
-                    className=""
-                    aria-label="Password requirements"
-                  >
-                    <InfoCircledIcon className="h-[17px] w-[17px]" />
-                  </button>
+                  {errors.password && (
+                    <InfoCircledIcon
+                      className="h-[17px] w-[17px] text-red-500"
+                      onMouseEnter={() => handleHover("password")}
+                      onMouseLeave={() => handleLeave("password")}
+                      aria-label="Password requirements"
+                    />
+                  )}
                 </Popover.Trigger>
                 <Popover.Portal>
                   <Popover.Content className="PopoverContent">
@@ -78,11 +138,15 @@ export const LoginTab = () => {
                 </Popover.Portal>
               </Popover.Root>
             </label>
-            <div className="flex h-[35px] items-center justify-between rounded-lg border border-white/20 pl-2">
+            <div
+              className={`flex h-[35px] items-center justify-between rounded-lg border border-white/20 pl-2 text-white/50 ${
+                (errors.password && "border-red-600/90 text-red-500") || ""
+              }`}
+            >
               <input
-                className="w-full bg-transparent text-white/50 focus:outline-none"
-                id="username"
+                className="w-full bg-transparent focus:outline-none"
                 type={showPassword ? "text" : "password"}
+                {...register("password")}
               />
               {showPassword ? (
                 <button type="button" onClick={() => setShowPassword(false)}>
