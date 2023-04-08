3b07d3de3ebdf5122a7ae62243db60f1ad56a2f4
Peter Yoo <trustintofu@gmail.com>
2023-04-08 04:15:45 -0700

    style(Home): Updates text, border, and bg colors. Updates font face for site title.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,14 @@ const Home: NextPage = () => {
   // -------------------------------------- JSX -------------------------------------- ***
   return (
     <>
-      <main className="flex h-full w-full flex-grow bg-gradient-to-tr from-[#2A2B2E] to-[#3E3E42]">
+      <main className="flex h-full w-full flex-grow">
         <div className="flex flex-grow bg-[url('https://res.cloudinary.com/dryh1nvhk/image/upload/v1680749458/NoteDown%20AI/landing-bg_n5bcpn.jpg')] bg-cover bg-center bg-no-repeat">
           <div className="flex h-full w-full items-center justify-center overflow-hidden">
-            <div className="flex h-full w-full flex-col items-center gap-5 rounded-lg bg-white/10 backdrop-blur-sm">
-              <h1 className="flex justify-center pt-12 text-5xl font-extrabold text-[#313133]">
+            <div className="flex h-full w-full flex-col items-center gap-5 rounded-lg bg-[#EEE6E1] bg-opacity-30 backdrop-blur-sm">
+              <h1 className="flex justify-center pt-12 font-serif text-5xl font-bold italic text-[#353432]">
                 NoteDown AI
               </h1>
-              <p className="h-[35px] max-w-[300px] text-center">
+              <p className="h-[35px] max-w-[300px] text-center text-[#353432]">
                 <TypingText
                   texts={[
                     "Our app employs generative ai that automatically takes notes for you, saving you time and effort.",
