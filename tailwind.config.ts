d078006f9de395df05877fd90fe9380d281eefb8
Peter Yoo <trustintofu@gmail.com>
2023-04-08 23:43:26 -0700

    feat(tailwind.config.ts): Adds darkMode prop.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import { type Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      serif: ['Times New Romans'],
+      serif: ["Times New Romans"],
       inter: ["var(--font-inter)"],
     },
   },
