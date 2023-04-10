7cee6eed0fcf955fe333761a7411683373fcc64a
Peter Yoo <trustintofu@gmail.com>
2023-04-10 01:26:24 -0700

    feat(TabsContainer): Creates tabsTrigger variable for tabs trigger components.

--- src/components/landing-page/TabsContainer.tsx
+++ src/components/landing-page/TabsContainer.tsx
@@ -5,6 +5,10 @@ import { SignupTab } from "./signup-tab/SignupTab.components";
 
 // -------------------------------- Component Function ------------------------------- ***
 export const TabsContainer = () => {
+  // ----------------------------------- Constants ----------------------------------- ***
+  const tabsTrigger =
+    "px-[20px] h-[45px] flex-grow flex items-center justify-center text-[15px] leading-4 text-[#565452] select-none transition-shadow duration-500 ease-in-out data-[state=active]:text-[#565452] data-[state=active]:bg-[#EEE6E1] data-[state=active]:shadow-[#565452] data-[state=active]:shadow-[0_2px_0px_0px] data-[state=active]:-mb-1 data-[state=active]:shadow-md first:rounded-tl-lg last:rounded-tr-lg focus:relative";
+
   // -------------------------------------- JSX -------------------------------------- ***
   return (
     <>
@@ -16,10 +20,10 @@ export const TabsContainer = () => {
           className="flex justify-between"
           aria-label="Manage your account"
         >
-          <Tabs.Trigger className="TabsTrigger" value="tab1">
+          <Tabs.Trigger className={`${tabsTrigger}`} value="tab1">
             Login
           </Tabs.Trigger>
-          <Tabs.Trigger className="TabsTrigger" value="tab2">
+          <Tabs.Trigger className={`${tabsTrigger}`} value="tab2">
             Sign Up
           </Tabs.Trigger>
         </Tabs.List>
