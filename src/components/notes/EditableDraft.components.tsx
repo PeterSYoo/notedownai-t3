d84063333c5e8a9fb6b290446b1cc36b54e64b20
Peter Yoo <trustintofu@gmail.com>
2023-04-14 22:33:56 -0700

    feat(EditableDraft): Fetches data from json file and adds content from json as tiptap nodes.

--- src/components/notes/EditableDraft.components.tsx
+++ src/components/notes/EditableDraft.components.tsx
@@ -1,20 +1,62 @@
 // -------------------------------------- Imports ------------------------------------ ***
-
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useEffect, useState } from "react";
 
 // -------------------------------- Component Function ------------------------------- ***
 export const EditableDraft = () => {
   // ------------------------------------- States ------------------------------------ ***
-  const editor = useEditor({
+  const [data, setData] = useState<any>(null);
+
+  const editorH1 = useEditor({
     extensions: [StarterKit],
-    content: "<p>Hello World!</p>",
+    content: "",
   });
+
+  const editorH3 = useEditor({
+    extensions: [StarterKit],
+    content: "",
+  });
+
+  const editorP = useEditor({
+    extensions: [StarterKit],
+    content: "",
+  });
+
+  // ------------------------------------ Effects ------------------------------------ ***
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../../data.json");
+        const data = await response.json();
+
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data !== null) {
+      editorH1.commands.setContent(`<h1>${data?.h1}</h1>`);
+      editorH3.commands.setContent(`<h3>${data?.h3}</h3>`);
+      editorP.commands.setContent(`<p>${data?.p}</p>`);
+    }
+  }, [data]);
+
+  if (data === null) return <>Data loading...</>;
 
   // -------------------------------------- JSX -------------------------------------- ***
   return (
     <>
-      <EditorContent editor={editor} />
+      <section className="flex flex-col gap-3">
+        <EditorContent editor={editorH1} />
+        <EditorContent editor={editorH3} />
+        <EditorContent editor={editorP} />
+      </section>
     </>
   );
 };
