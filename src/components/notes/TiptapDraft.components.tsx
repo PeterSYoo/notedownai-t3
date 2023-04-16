2299cdd5fdb2bd28592144e434fddd1b2dccd0f5
Peter Yoo <trustintofu@gmail.com>
2023-04-15 21:51:44 -0700

    feat(TiptapDraft): Creates testing area for tiptap nodes.

---
+++ src/components/notes/TiptapDraft.components.tsx
@@ -0,0 +1,261 @@
+import { Color } from "@tiptap/extension-color";
+import ListItem from "@tiptap/extension-list-item";
+import TextStyle from "@tiptap/extension-text-style";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import React from "react";
+
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        className={
+          editor.isActive("bold")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        bold
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={
+          editor.isActive("italic")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        italic
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        disabled={!editor.can().chain().focus().toggleStrike().run()}
+        className={
+          editor.isActive("strike")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        strike
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        disabled={!editor.can().chain().focus().toggleCode().run()}
+        className={
+          editor.isActive("code")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        code
+      </button>
+      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+        clear marks
+      </button>
+      <button onClick={() => editor.chain().focus().clearNodes().run()}>
+        clear nodes
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        className={
+          editor.isActive("paragraph")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        paragraph
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={
+          editor.isActive("heading", { level: 1 })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        h1
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={
+          editor.isActive("heading", { level: 2 })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        h2
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={
+          editor.isActive("heading", { level: 3 })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        h3
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={
+          editor.isActive("heading", { level: 4 })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        h4
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={
+          editor.isActive("heading", { level: 5 })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        h5
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        className={
+          editor.isActive("heading", { level: 6 })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        h6
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={
+          editor.isActive("bulletList")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        bullet list
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={
+          editor.isActive("orderedList")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        ordered list
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={
+          editor.isActive("codeBlock")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        code block
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={
+          editor.isActive("blockquote")
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        blockquote
+      </button>
+      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        horizontal rule
+      </button>
+      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+        hard break
+      </button>
+      <button
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+      >
+        undo
+      </button>
+      <button
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+      >
+        redo
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setColor("#958DF1").run()}
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" })
+            ? "rounded-lg border border-black p-2 dark:border-white"
+            : ""
+        }
+      >
+        purple
+      </button>
+    </>
+  );
+};
+
+export default () => {
+  const editor = useEditor({
+    extensions: [
+      Color.configure({ types: [TextStyle.name, ListItem.name] }),
+      TextStyle.configure({ types: [ListItem.name] }),
+      StarterKit.configure({
+        bulletList: {
+          keepMarks: true,
+          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+        orderedList: {
+          keepMarks: true,
+          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+      }),
+    ],
+    content: `
+      <h2>
+        Hi there,
+      </h2>
+      <p>
+        this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+      </p>
+      <ul>
+        <li>
+          That’s a bullet list with one …
+        </li>
+        <li>
+          … or two list items.
+        </li>
+      </ul>
+      <p>
+        Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+      </p>
+      <pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+      <p>
+        I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+      </p>
+      <blockquote>
+        Wow, that’s amazing. Good work, boy! 👏
+        <br />
+        — Mom
+      </blockquote>
+    `,
+  });
+
+  return (
+    <div>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
